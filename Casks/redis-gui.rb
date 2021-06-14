05f6ba3ee0e6f5f1c45757838543992c64bca3e4
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2021-06-14 14:44:40 +0200

    Revert to v1.2.0 wich still has proper release files

--- Casks/redis-gui.rb
+++ Casks/redis-gui.rb
@@ -1,9 +1,9 @@
 
 cask 'redis-gui' do
-  version '1.2.2'
-  sha256 'b528aa66a5dba1c5ce810f7cd410d8f54f4527d5b700d5f93e1d0bbdf579b225'
+  version '1.2.0'
+  sha256 'fe5a34a9855c5aa99e09af9f885ba23c6d7bc6275c2225049750439421c3b501'
 
-  url 'https://github.com/ekvedaras/redis-gui/archive/refs/tags/v1.2.2.zip'
+  url 'https://github.com/ekvedaras/redis-gui/releases/download/v1.2.0/redis-gui-1.2.0-mac.zip'
   appcast 'https://github.com/ekvedaras/redis-gui/releases.atom'
   name 'Redis GUI (unofficial)'
   homepage 'https://github.com/ekvedaras/redis-gui'
