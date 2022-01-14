50a0dd779a2b1e91aa103e493f689eb4cd316112
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2022-01-14 14:58:44 +0100

    v1.2.11

--- Casks/redis-gui.rb
+++ Casks/redis-gui.rb
@@ -1,9 +1,9 @@
 
 cask 'redis-gui' do
-  version '1.2.10'
-  sha256 '9b61160c30ba1b83c2be475924f210b532c13802dfa892955b11f6ac20eefff2'
+  version '1.2.11'
+  sha256 '983d7ce949ad00b58c45b99a05bb6d9c85bbd4e95530b600af818eb4090d973f'
 
-  url 'https://github.com/ekvedaras/redis-gui/releases/download/v1.2.10/redis-gui-1.2.10-mac.zip'
+  url 'https://github.com/ekvedaras/redis-gui/releases/download/v1.2.11/redis-gui-1.2.11-mac.zip'
   appcast 'https://github.com/ekvedaras/redis-gui/releases.atom'
   name 'Redis GUI (unofficial)'
   homepage 'https://github.com/ekvedaras/redis-gui'
