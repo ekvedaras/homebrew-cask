fa7f89d98e7d611bfdc60edc75ebc1e15a26eb13
Ernestas Kvedaras <kvedarasernestas@gmail.com>
2022-07-31 10:09:55 +0200

    Update redis-gui.rb

--- Casks/redis-gui.rb
+++ Casks/redis-gui.rb
@@ -1,10 +1,10 @@
 
 cask 'redis-gui' do
-  version '2.1.1'
+  version '2.2.0'
   
 #   if Hardware::CPU.intel?
-    url 'https://github.com/ekvedaras/redis-gui/releases/download/v2.1.1/redis-gui-2.1.1-mac.zip'
-    sha256 '2c63dd2fd768b53a6b775b55b2c9f4bec65bae324270082c6415ad53376634fa'
+    url 'https://github.com/ekvedaras/redis-gui/releases/download/v2.2.0/redis-gui-2.2.0-mac.zip'
+    sha256 '1b7a0d774473c65d933a9c5ec815d3ab97c4d754e96bd2dc2daff1e118b3794c'
 #     "x64"
 #   else
 #     url 'https://github.com/ekvedaras/redis-gui/releases/download/v2.0.0/redis-gui-2.0.0-arm64-mac.zip'
